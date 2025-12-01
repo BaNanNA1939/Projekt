0e074ca7ee5f2a825235f114bb1a9260778b8587
BaNanNA1939 <darshandevadiga2022@outlook.com>
2025-12-01 20:31:22 +0530

    update

---
+++ home/main.js
@@ -0,0 +1,9 @@
+console.log('Hello World!');
+
+document.getElementById('btn1').onclick = function chk(){
+  console.log('good');
+}
+
+document.getElementById('btn2').onclick = function chk(){
+  console.log('good2');
+}
