6fdc4895af65683f685bab621926c170607968da
BaNanNA1939 <darshandevadiga2022@outlook.com>
2025-12-02 13:02:28 +0530

    update main.js

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ console.log('Hello World!');
 function dev() {
   console.log ('No page.');
   console.log ('Redircting to construct site...');
-  window.location.href = 'https://bananna1939.github.io/Projekt//Development/index.html'
+  window.location.href = 'dev.html'
 }
