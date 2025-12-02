5239a947098e9d63d91cf55f90de7070eb0ccd85
BaNanNA1939 <darshandevadiga2022@outlook.com>
2025-12-02 12:15:43 +0530

    user friendly

---
+++ Development/main.js
@@ -0,0 +1,6 @@
+console.log('Hello World!');
+
+document.getElementById('go').onclick = function () {
+  console.log('Redirecting...');
+  window.location.href = "/index.html";
+}
--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 console.log('Hello World!');
 
+function dev() {
+  console.log ('No page.');
+  console.log ('Redircting to construct site...');
+  window.location.href = '/development/index.html'
+}
