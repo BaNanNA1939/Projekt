d16600e589099d697951290afdad736f561823dd
BaNanNA1939 <darshandevadiga2022@outlook.com>
2025-12-02 12:18:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ console.log('Hello World!');
 function dev() {
   console.log ('No page.');
   console.log ('Redircting to construct site...');
-  window.location.href = '/development/index.html'
+  window.location.href = '/Development/index.html'
 }
